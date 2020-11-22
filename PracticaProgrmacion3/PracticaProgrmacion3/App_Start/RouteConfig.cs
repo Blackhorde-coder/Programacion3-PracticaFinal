ac819d95e64b5ec395358e34acba6849e685c0c9
Blackhorde <esingcomputacion@gmail.com>
2020-11-22 20:35:04 -0300

    2

--- PracticaProgrmacion3/PracticaProgrmacion3/App_Start/RouteConfig.cs
+++ PracticaProgrmacion3/PracticaProgrmacion3/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PracticaProgrmacion3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Curriculum", action = "Curriculum", id = UrlParameter.Optional }
             );
         }
     }
---
+++ PracticaProgrmacion3/PracticaProgrmacion3/Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaProgrmacion3.Models
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private int documento;
+
+        public Persona(string nombre, string apellido, int edad, int documento)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Edad = edad;
+            this.Documento = documento;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Documento { get => documento; set => documento = value; }
+    }
+}
